8c9184e135c23d8aa360861a32e1d9fb2f972d86
neelesh32008 <38097596+neelesh32008@users.noreply.github.com>
2019-06-30 13:22:56 +0530

    bcrypt Hash

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser =require("body-parser");
 const mongoose =require("mongoose");
-const encrypt =require("mongoose-encryption");
+const bcrypt =require("bcrypt");
+const saltRounds =10;
 
 const app =express();
 
@@ -18,8 +19,6 @@ const userSchema =new mongoose.Schema({
   password:String
 });
 
-
-userSchema.plugin(encrypt ,{secret:process.env.SECRET, encryptedFields:["password"] });
 
 const User =new mongoose.model("User", userSchema);
 
@@ -36,35 +35,40 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
+
+bcrypt.hash(req.body.password, saltRounds, function(err, hash){
   const newUser =new User({
     email:req.body.username,
-    password:req.body.password
+    password:hash
   });
   newUser.save(function(err){
     if(err){
       console.log(err);
     }else{
-        res.render("/secrets");
+        res.render("secrets");
     }
+});
+
   });
 
 });
 
 
-app.post("/login",function(req,res){
+app.post("/login",  function(req,res){
   const username =req.body.username;
   const password =req.body.password;
 
   User.findOne({email:username}, function(err, foundUser){
-
-    if(err){
+      if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          console.log(password);
-          res.render("/secrets");
-        }
+    bcrypt.compare(password, foundUser.password, function(err, result){
+      if(result === true){
+          res.render("secrets");
+      }
+    });
+
       }
     }
   });
